e7aefdc83f3100ca4f3080ee817838efe4e1c3df
Vedu Mallela <42554579+vmallela0@users.noreply.github.com>
2020-11-24 15:45:33 -0500

    added colors, readme, resolution

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import subprocess
 from tkinter import *
 from tkinter.filedialog import askopenfile
 from tkinter.filedialog import askdirectory
+from tkinter.colorchooser import askcolor
 from tkinter.ttk import *
 
 root = Tk()
@@ -41,22 +42,22 @@ def cpLocal(source, destination): # copy and paste for docker
 def sedDocker(setting_old, setting_new, line_number): # changes config.py on docker side
     return(f'''docker exec -it {container.id} sed -i "{line_number} s|{setting_old}|{setting_new}|" /home/brain-coloring/config.py''')
 
-def convert_2_bpmodes(angle, mode): 
+def convert_2_bpmodes(angle, mode):
     # add the view/angle config.py information
-    
-    if angle == 'Top': 
+
+    if angle == 'Top':
         return sedDocker("IMG_TYPE = 'cortical-outer-right-hemisphere'", "IMG_TYPE = 'top'", 15)
-    elif angle == 'Bottom': 
+    elif angle == 'Bottom':
         return sedDocker("IMG_TYPE = 'cortical-outer-right-hemisphere'", "IMG_TYPE = 'bottom'", 15)
     elif mode == 'Cortical-Outer':
-        if 'Right-Hemisphere' == angle: 
+        if 'Right-Hemisphere' == angle:
             return sedDocker("IMG_TYPE = 'cortical-outer-right-hemisphere'", "IMG_TYPE = 'cortical-outer-right-hemisphere'", 15)
-        elif 'Left-Hemisphere' == angle: 
+        elif 'Left-Hemisphere' == angle:
             return sedDocker("IMG_TYPE = 'cortical-outer-right-hemisphere'", "IMG_TYPE = 'cortical-outer-left-hemisphere'", 15)
     elif mode == 'Cortical-Inner':
-        if 'Right-Hemisphere' == angle: 
+        if 'Right-Hemisphere' == angle:
             return sedDocker("IMG_TYPE = 'cortical-outer-right-hemisphere'", "IMG_TYPE = 'cortical-inner-right-hemisphere'", 15)
-        elif 'Left-Hemisphere' == angle: 
+        elif 'Left-Hemisphere' == angle:
             return sedDocker("IMG_TYPE = 'cortical-outer-right-hemisphere'", "IMG_TYPE = 'cortical-inner-left-hemisphere'", 15)
     elif mode == 'Subcortical':
         return sedDocker("IMG_TYPE = 'cortical-outer-right-hemisphere'", "IMG_TYPE = 'subcortical'", 15)
@@ -90,10 +91,20 @@ class Gui:
             dataOutputLabel.config(font =("Helvicta", 14))
             dataOutputLabel.grid(row = row_number, column = 1, sticky = W, pady = 2, padx = 2)
 
+    def choose_color(self, val, display, grid_num):
+        # variable to store hexadecimal code of color
+        result = askcolor(title = "Color Chooser")
+        blender_color_value = [color/256 for color in result[0]]
+        print(blender_color_value)
+        display = Label(root, text = str(blender_color_value))
+        display.grid(row = grid_num, column = 1, sticky = W, pady = 2)
+        self.colors[val] = blender_color_value
+
     def __init__(self, instance_name):
         self.name = instance_name
         self.dataInput = ''
         self.dataOutput = ''
+        self.colors = [[1,1,1], [1,1,0], [1,0.4,0], [1,0,0]]
 
     def run(self):
         menubar = Menu(root)
@@ -164,13 +175,67 @@ class Gui:
         mode_chooser_label = Label(root, text = "Choose mode:")
         mode_chooser_label.grid(row = 9, column = 0, sticky = W, pady = 2)
 
+        # color picker
+
+        # c0
+        color_0 = Menu(menubar, tearoff=0)
+        color_0 = Button(root, text ='Pick color 0', command = lambda:self.choose_color(0, color_0_display, 11))
+        color_0.grid(row = 10, column = 1, sticky = W, pady = 2, padx = 2)
+
+        color_0_label = Label(root, text = "Choose biomarker value 0:")
+        color_0_label.grid(row = 10, column = 0, sticky = W, pady = 2)
+
+        color_0_display = Label(root, text = str(self.colors[0]))
+        color_0_display.grid(row = 11, column = 1, sticky = W, pady = 2)
+
+        # c1
+        color_1 = Menu(menubar, tearoff=0)
+        color_1 = Button(root, text ='Pick color 1', command = lambda:self.choose_color(1, color_1_display, 13))
+        color_1.grid(row = 12, column = 1, sticky = W, pady = 2, padx = 2)
+
+        color_1_label = Label(root, text = "Choose biomarker value 1:")
+        color_1_label.grid(row = 12, column = 0, sticky = W, pady = 2)
+
+        color_1_display = Label(root, text = str(self.colors[1]))
+        color_1_display.grid(row = 13, column = 1, sticky = W, pady = 2)
+
+        # c2
+        color_2 = Menu(menubar, tearoff=0)
+        color_2 = Button(root, text ='Pick color 2', command = lambda:self.choose_color(2, color_2_display, 15))
+        color_2.grid(row = 14, column = 1, sticky = W, pady = 2, padx = 2)
+
+        color_2_label = Label(root, text = "Choose biomarker value 2:")
+        color_2_label.grid(row = 14, column = 0, sticky = W, pady = 2)
+
+        color_2_display = Label(root, text = str(self.colors[2]))
+        color_2_display.grid(row = 15, column = 1, sticky = W, pady = 2)
+
+        # c3
+        color_3 = Menu(menubar, tearoff=0)
+        color_3 = Button(root, text ='Pick color 3', command = lambda:self.choose_color(3, color_3_display, 17))
+        color_3.grid(row = 16, column = 1, sticky = W, pady = 2, padx = 2)
+
+        color_3_label = Label(root, text = "Choose biomarker value 3:")
+        color_3_label.grid(row = 16, column = 0, sticky = W, pady = 2)
+
+        color_3_display = Label(root, text = str(self.colors[3]))
+        color_3_display.grid(row = 17, column = 1, sticky = W, pady = 2)
+
+        # resolution
+        resolution = Menu(menubar, tearoff=0)
+        resolution = Entry(root, textvariable = "Resolution",font=('Helvicta',14))
+        resolution.insert(0, "1200,900")
+        resolution.grid(row = 18, column = 1, sticky = W, pady = 2)
+
+        resolution_label = Label(root, text = "Resolution:")
+        resolution_label.grid(row = 18, column = 0, sticky = W, pady = 2)
 
         # run button
         run = Menu(menubar, tearoff=0)
-        run = Button(root, text="Run Brainpainter", command = lambda: self.run_docker(self.dataInput, self.dataOutput, output_folder.get(), brain_type.get(), atlas.get(), img_angle.get(), mode.get())) # print([self.dataInput, self.dataOutput, brain_type.get(), atlas.get(), img_angle.get(), mode.get()])
-        run.grid(row = 10, column = 1, sticky = W, pady = 2, padx = 2)
+        run = Button(root, text="Run Brainpainter", command = lambda: self.run_docker(self.dataInput, self.dataOutput, output_folder.get(), brain_type.get(), atlas.get(), img_angle.get(), mode.get(), self.colors, resolution.get()))
+        run.grid(row = 20, column = 1, sticky = W, pady = 2, padx = 2)
 
-    def run_docker(self, input_location, output_location, output_name, brain_type, atlas, img_angle, mode):
+    def run_docker(self, input_location, output_location, output_name, brain_type, atlas, img_angle, mode, colors, resolution):
         try:
             container.start()
             # os.system('docker ps --no-trunc')
@@ -185,9 +250,6 @@ class Gui:
 
             OUTPUT_DOCKER = ':/home/brain-coloring/output/' + output_name
             cmdExtract = cpLocal(OUTPUT_DOCKER, output_location)
-
-            mode_chooser_label = Label(root, text = "Oops! Something went wrong. Make sure you filled out the values")
-            mode_chooser_label.grid(row = 11, column = 1, sticky = W, pady = 2, padx = 2)
 
             print(cmdCopy)
             subprocess.Popen(cmdCopy, shell=True).wait() # copying inputs into docker container
@@ -209,19 +271,26 @@ class Gui:
             print(convert_2_bpmodes(img_angle, mode))
             os.system(convert_2_bpmodes(img_angle, mode)) # changing the mode/angle
 
+            print(sedDocker("# default is white", "COLORS_RGB = " + str(colors), 29))
+            os.system(sedDocker("# default is white", "COLORS_RGB = " + str(colors), 29)) # changing colors
+            
+            print(sedDocker("RESOLUTION = (1200, 900)", "RESOLUTION = (" + resolution + ")", 27))
+            os.system(sedDocker("RESOLUTION = (1200, 900)", "RESOLUTION = (" + resolution + ")", 27))
+
             print(cmdRun)
             subprocess.Popen(cmdRun, shell=True).wait() # generating images
 
             print(cmdExtract)
             subprocess.Popen(cmdExtract, shell=True).wait() # moving images from docker to local
 
+            mode_chooser_label = Label(root, text = "Images generated")
+            mode_chooser_label.grid(row = 21, column = 1, sticky = W, pady = 2, padx = 2)
         except:
             print("raised")
             mode_chooser_label = Label(root, text = "Oops! Something went wrong. Make sure you filled out the values")
-            mode_chooser_label.grid(row = 11, column = 1, sticky = W, pady = 2, padx = 2)
+            mode_chooser_label.grid(row = 21, column = 1, sticky = W, pady = 2, padx = 2)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # tkinter init
     gui = Gui("main")
     gui.run()
---
+++ readme.md
@@ -0,0 +1,23 @@
+# BrainPainter 
+
+This repository holds the code for a GUI for running [BrainPainter](https://brainpainter.csail.mit.edu). It acts as an interface for Docker and loads the configuration file with your preferences. 
+
+## Installation
+
+To install this software please go to releases on the side. 
+
+If you'd like to run the code please run to get all of the dependencies.
+```bash
+pip install requirements.txt
+```
+
+## Usage
+When running this software please make sure that your Docker is up and running. If you have any problems feel free to [contact](www.vmallela.com/contact) me. If you don't have many templates to load the
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
